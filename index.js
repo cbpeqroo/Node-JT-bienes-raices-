aa7e9c4e991644c0659268343b2a2e2cbcda92c4
Shoice <shoice.esparza@gmail.com>
2024-12-19 23:29:50 -0500

    agregando Routingn

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express')
 // Crear la app
 const app = express()
 
-
+// Routing
+app.get('/', function(req, res){
+    res.send('Hola mundo desde express')
+})
 
 // Definir el puerto y arrancar el proyecto
 const port = 3000;
