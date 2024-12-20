fc61836ffe9edb5c1cff5bccff897d3cd8d9035f
Shoice <shoice.esparza@gmail.com>
2024-12-20 00:42:19 -0500

    probando peticiones get y post

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -4,14 +4,20 @@ const router = express.Router();
 
 
 // Routing
-router.get('/', function(req, res){
-    res.send('Hola mundo desde express')
+router.get('/', (req, res) =>{
+    res.json('Hola mundo desde express')
 });
 
 
-router.get('/nosotros', function(req, res){
-    res.send('Información de nosotros')
+router.post('/', (req, res) =>{
+    res.json({msg: 'Respuesta de tipo Post'})
 });
+
+
+
+
+
+
 
 
 export default router
